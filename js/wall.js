eb47163c2e3eedf30179cf0e4b1ac63e7eebd0c7
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-07-23 07:31:24 +0200

    Websocket keep alive

--- js/wall.js
+++ js/wall.js
@@ -1,40 +1,37 @@
-$(document).ready(function() {
-	var wsCommand = new WebSocket("ws://" + location.host + "/websocket");
-
-	function changeSlide(image) {
-		$("#slide").attr("src", image);
-	}
-	
-	// Receiving a message from the server
-	wsCommand.onmessage = function (message) {
-		//console.log("receiving: " + message.data);
+function OpenWebSocket() {
+	websocket = new WebSocket("ws://" + location.host + "/websocket");
+	websocket.onmessage = function (message) {
+		console.log("receiving: " + message.data);
 		obj = JSON.parse(message.data);
 
 		if (obj.cmd=="slide") {
 			changeSlide(obj.slide);
 		}
-
-		if (obj.cmd=="pos") {
-			console.log(" ");
-			console.log("Ricevuto:",obj.x,obj.y);
-			console.log("Dimensioni immagine",$("#slide").width(),$("#slide").height());
-			kx=$("#slide").width()/100*obj.x;
-			ky=$("#slide").height()/100*obj.y;
-			console.log("Posizione x,y:",kx,ky);
-			
-			$("#target").css("left",kx-$("#target").width()/2);
-			$("#target").css("top",ky-$("#target").height()/2);
-			$("#target").fadeIn(400).fadeOut(1500);
-		}
 	};
 	
-	wsCommand.onopen = function() 	{
+	websocket.onopen = function() 	{
 		console.log("opened"); 
 	};
-
-	wsCommand.onclose = function() 	{
+	websocket.onclose = function() 	{
 		console.log("disconnected"); 
 	};
+	return websocket;
+}
 
-	
+function changeSlide(image) {
+	$("#slide").attr("src", image);	
+}
+
+$(document).ready(function() {
+	websocket=OpenWebSocket();
+
+	setInterval(function(){
+		if (websocket.readyState > 1) {
+			console.log("is closed");
+			websocket=OpenWebSocket();
+		} else {
+			console.log("is open");
+		}			
+	},3000);
+
 });	
