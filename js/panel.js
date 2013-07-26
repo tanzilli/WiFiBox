8d914b5a7b264e709b13f422a7bb915e08ac5ca4
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-07-26 12:20:06 +0200

    Auto proporzionato

--- js/panel.js
+++ js/panel.js
@@ -1,6 +1,7 @@
 // Open a websocket with the server
 var wsCommand = new WebSocket("ws://" + location.host + "/websocket");
 var thumbSize=128;
+var slideList=[];
 
 function liveAll() {
 	wsCommand.send('{"cmd":"titolo_1","text":"' + $("#titolo_1_text").val() + '"}');
@@ -11,8 +12,8 @@ function sendSlide(slide) {
 	wsCommand.send('{"cmd":"slide","slide":"/artisti/' + slide + '"}');
 }
 
-function ShowThumbnail(slideslist) {
-	spazio_disponibile=$(document).width();
+function RefreshThumbnail(slideslist) {
+	spazio_disponibile=$(document).width()-128;
 	thumbPerRiga=Math.floor(spazio_disponibile/thumbSize);
 
 	htmlcontents="";
@@ -26,9 +27,20 @@ function ShowThumbnail(slideslist) {
 		}
 	}	
 	$("#thumbnaillist").html(htmlcontents);
+
 	$(".slide").click(function() {
-		wsCommand.send('{"cmd":"slide","slide":"' + $(this).attr("src") + '"}');
+		wsCommand.send('{"cmd":"slide","slide":"' + "/artisti/" + $(this).attr("title") + '"}');
 	});
+}
+
+function ShowThumbnail(slideslist) {
+	RefreshThumbnail(slideslist);
+
+	$(window).resize(function() {
+		console.log($(document).width());
+		RefreshThumbnail(slideslist);
+	});
+	
 	$("#titolo_1_live").click(function() {
 		wsCommand.send('{"cmd":"titolo_1","text":"' + $("#titolo_1_text").val() + '"}');
 	});
@@ -36,10 +48,9 @@ function ShowThumbnail(slideslist) {
 		wsCommand.send('{"cmd":"titolo_2","text":"' + $("#titolo_2_text").val() + '"}');
 	});
 	$(".select_text").click(function() {
-		//alert($(this).find('option:selected').text());
 		titolo_1=$(this).find('option:selected').text();
 		titolo_2=$(this).val();
-		if (titolo_1=="Blank") {
+		if (titolo_2=="notext") {
 			$("#titolo_1_text").val("");
 			$("#titolo_2_text").val("");
 		} else {
@@ -51,7 +62,7 @@ function ShowThumbnail(slideslist) {
 		if (slide_name!=undefined) {
 			sendSlide(slide_name);
 		} else {
-			sendSlide("banner.png");
+			sendSlide("banner1.jpg");
 		}	
 	});
 }
