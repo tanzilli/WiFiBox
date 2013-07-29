64e70ac5f42e98e4ac69f2cbd3263dc63654aa47
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-07-29 12:15:51 +0200

    Artist profile in one file

--- js/index.js
+++ js/index.js
@@ -1,14 +1,5 @@
 var websocket = new WebSocket("ws://" + location.host + "/websocket");
 
-console.log("Ci sono");
-
-$(document).ready(function() {
-	$(".include-file").each (function () {
-		$(this).load($(this).attr('file'));
-		console.log("Ciao");
-	});
-});
-
 websocket.onmessage=function(message) {
 	obj = JSON.parse(message.data);
 	if (obj.cmd=="poll") {
